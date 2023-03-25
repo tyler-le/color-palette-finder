d9f2036327285ec48b070c005b5aee505c49153c
Tyler Le <tylerle99@yahoo.com>
2023-03-25 13:57:22 -0700

    color block

--- popup.js
+++ popup.js
@@ -1,4 +1,25 @@
-chrome.runtime.sendMessage({ 'method': 'getColors' }, function (response) {
-    //response is now the info collected by the content script.
-    console.log(response);
+
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.runtime.sendMessage({ method: "getColors" }, function (response) {
+        // Sort the colors by frequency
+        const colorCounts = {};
+        response.forEach((color) => {
+            colorCounts[color] = (colorCounts[color] || 0) + 1;
+        });
+        const sortedColors = Object.keys(colorCounts).sort(
+            (a, b) => colorCounts[b] - colorCounts[a]
+        );
+
+        // Create blocks of the most frequent colors
+        const colorsContainer = document.getElementById("output");
+        for (let i = 0; i < 5 && i < sortedColors.length; i++) {
+            const colorBlock = document.createElement("div");
+            colorBlock.style.backgroundColor = sortedColors[i];
+            colorBlock.style.width = "50px";
+            colorBlock.style.height = "50px";
+            colorBlock.style.margin = "5px";
+            colorBlock.style.border = "1px solid black";
+            colorsContainer.appendChild(colorBlock);
+        }
+    });
 });
