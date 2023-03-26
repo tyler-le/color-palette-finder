fb3b7c2e079655dfee7a195012d131927cc3887a
Tyler Le <tylerle99@yahoo.com>
2023-03-26 11:41:16 -0700

    v1.0 working

--- popup.js
+++ popup.js
@@ -12,4 +12,24 @@ window.addEventListener('load', function () {
 });
 
 function renderColors(colors) {
+    // Sort the colors by frequency
+    const colorCounts = {};
+    colors.forEach((color) => {
+        colorCounts[color] = (colorCounts[color] || 0) + 1;
+    });
+    const sortedColors = Object.keys(colorCounts).sort(
+        (a, b) => colorCounts[b] - colorCounts[a]
+    );
+
+    // Create blocks of the most frequent colors
+    const colorsContainer = document.getElementById("output");
+    for (let i = 0; i < 5 && i < sortedColors.length; i++) {
+        const colorBlock = document.createElement("div");
+        colorBlock.style.backgroundColor = sortedColors[i];
+        colorBlock.style.width = "50px";
+        colorBlock.style.height = "50px";
+        colorBlock.style.margin = "5px";
+        colorBlock.style.border = "1px solid black";
+        colorsContainer.appendChild(colorBlock);
+    }
 }
