cd033cffdf5a2f64fb668d79d5acfc53895d5bb3
Tyler Le <tylerle99@yahoo.com>
2023-03-26 13:44:28 -0700

    handle hsl values

--- content_script.js
+++ content_script.js
@@ -19,6 +19,41 @@ function rgbToHex(rgb) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
+// Function to convert HSL color to HEX format
+function hslToHex(hsl) {
+    const hslValues = hsl.match(/\d+/g);
+    const h = parseInt(hslValues[0]);
+    const s = parseInt(hslValues[1]) / 100;
+    const l = parseInt(hslValues[2]) / 100;
+    let r, g, b;
+
+    if (s === 0) {
+        r = g = b = l;
+    } else {
+        const hueToRgb = (p, q, t) => {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        };
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hueToRgb(p, q, h / 360 + 1 / 3);
+        g = hueToRgb(p, q, h / 360);
+        b = hueToRgb(p, q, h / 360 - 1 / 3);
+    }
+
+    const toHex = (c) => {
+        const hex = Math.round(c * 255).toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    };
+
+    return "#" + toHex(r) + toHex(g) + toHex(b);
+}
+
+
 function getColors() {
     // Regular expression to match colors in CSS
     const colorRegex = /#([0-9a-f]{3}){1,2}\b|\b((rgb|hsl)a?\([\d\s%,.]+\))/gi;
@@ -49,7 +84,12 @@ function getColors() {
                             if (colorValue.startsWith("rgb")) {
                                 const hexValue = rgbToHex(colorValue);
                                 colors.push(hexValue);
-                            } else {
+                            }
+                            else if (colorValue.startsWith("hsl")) {
+                                const hexValue = hslToHex(colorValue);
+                                colors.push(hexValue);
+                            }
+                            else {
                                 colors.push(colorValue);
                             }
                         });
